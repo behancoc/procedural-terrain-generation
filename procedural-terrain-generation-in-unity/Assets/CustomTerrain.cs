40c7abbb52eca3f4bd67d55a162e56924e44c63a
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-02-29 13:50:47 -0800

    Adds new scene with terrain object; Custom editor to manipulate terrain object

---
+++ procedural-terrain-generation-in-unity/Assets/CustomTerrain.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+//Allows code to run in editor mode
+[ExecuteInEditMode]
+
+public class CustomTerrain : MonoBehaviour
+{
+    void Awake()
+    {
+        SerializedObject tagManager = new SerializedObject(
+            AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+        SerializedProperty tagsProp = tagManager.FindProperty("tags");
+
+        AddTag(tagsProp, "Terrain");
+        AddTag(tagsProp, "Cloud");
+        AddTag(tagsProp, "Shore");
+
+        //Apply tag changes to tag database;
+        tagManager.ApplyModifiedProperties();
+
+        //take this object
+        //this.gameObject = "Terrain";
+        gameObject.tag = "Terrain";
+    }
+
+    void AddTag(SerializedProperty tagsProp, string newTag)
+    {
+        bool found = false;
+        
+        //ensure the tag doesn't already exist
+        for(int i = 0; i < tagsProp.arraySize; i++)
+        {
+            SerializedProperty t = tagsProp.GetArrayElementAtIndex(i);
+            if (t.stringValue.Equals(newTag))
+            {
+                found = true;
+                break;
+            }
+        }
+
+        //add my tag
+        if(!found)
+        {
+            tagsProp.InsertArrayElementAtIndex(0);
+            SerializedProperty newTagProp = tagsProp.GetArrayElementAtIndex(0);
+            newTagProp.stringValue = newTag;
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
